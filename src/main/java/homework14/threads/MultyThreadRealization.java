d59c572b6d05ff1cd7acd0b5aa331cb0327814c3
Vsinitsyn <sinicyn.vladimir1985@gmail.com>
2025-01-31 21:04:01 +0300

    Introducing to Threads

---
+++ src/main/java/homework14/threads/MultyThreadRealization.java
@@ -0,0 +1,33 @@
+package homework14.threads;
+
+public class MultyThreadRealization {
+    public static void main(String[] args) throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        System.out.println("Начало программы" + startTime);
+        Thread[] threads = new Thread[4];
+        double[] array = new double[1000000000];
+        for (int t = 0; t < threads.length; t++) {
+            int threadId = t;
+            threads[threadId] = new Thread(() -> {
+
+                for (int i = threadId * 250000; i < (threadId + 1) * 250000; i++) {
+                    array[i] = 1.14 * Math.cos(i) * Math.sin(i * 0.2) * Math.cos(i / 1.2);
+                }
+            });
+
+            threads[t].start();
+
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Время окончания выполнения программы " + endTime);
+        System.out.println("Время выполнения в 4 потока: " + (endTime - startTime));
+    }
+
+}
+
+
+
+
